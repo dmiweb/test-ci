b238f29996bca9ed11072dc9207414f766153b50
dmiweb <dimbons@mail.ru>
2024-03-31 13:33:21 +0300

    homework correction

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+import indicatorHealth from '../js/indicatorHealth';
+import sortUserHealth from '../js/sortUserHealth';
+import getLevel from '../js/getLevel';
+/* eslint-disable no-unused-vars */
--- src/js/http.js
+++ src/js/http.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line no-unused-vars
 export default function fetchData(url) {
   throw new Error('Mock this!');
 }
+
